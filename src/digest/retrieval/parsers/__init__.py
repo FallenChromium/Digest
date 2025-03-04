0f7b121e49540099050dae42bda07088a384ee84
FallenChromium <fallenchromium@gmail.com>
2025-03-04 18:03:17 +0300

    fix: fix tchan discoverability in ParserRegistry

--- src/digest/retrieval/parsers/__init__.py
+++ src/digest/retrieval/parsers/__init__.py
@@ -4,5 +4,5 @@ Parsers for various types of news sources.
 
 from digest.retrieval.parsers.base import BaseParser, ParserRegistry
 from digest.retrieval.parsers.rss import RssParser
-
+from digest.retrieval.parsers.tchan import TchanParser
 # Add more parser imports here as they are created 
